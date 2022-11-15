bb0430e4f757de81110c9ba018b7a676082151dd
Gregory Odiase <gregory.odiase@cwg-plc.com>
2022-11-15 10:13:36 +0100

    feat: added midbanner elements and styling

--- src/components/midbanner.js
+++ src/components/midbanner.js
@@ -1,9 +1,26 @@
+import '../styles/midbanner.css';
+
+import mbTokenLogo from '../assets/mbTokenLogo.svg';
+import metaMaskLogo from '../assets/metaMaskLogo.svg';
+import openSeaLogo from '../assets/openSeaLogo.svg';
+
 
 function MidBanner(){
     return (
-        <section style={{border : "solid"}}>
-            mid banner here
-        </section>
+        <div className="section theme-color-bg midBannerSec">
+            <div className='icon-div'>
+                <img src={mbTokenLogo} alt="mb Token"/>
+                <h2>MBToken</h2>
+            </div>
+            <div className='icon-div'>
+                <img src={metaMaskLogo} alt="meta mask" className='rs-half'/>
+                <h2> METAMASK</h2>
+            </div>
+            <div className='icon-div'>
+                <img src={openSeaLogo} alt="Open sea" className='rs-half'/>
+                <h2> OpenSea</h2>
+            </div>
+        </div>
     );
 }
 
