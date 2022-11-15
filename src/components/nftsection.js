cfa49b567bc34da6bfc8af7a416fe4185682144f
Gregory Odiase <gregory.odiase@cwg-plc.com>
2022-11-15 17:45:34 +0100

    feat: added nft section elements and styling

--- src/components/nftsection.js
+++ src/components/nftsection.js
@@ -1,9 +1,30 @@
+import '../styles/nftsection.css';
+
+import nftImg from '../assets/nftLogo.svg';
 
 function NFTSection(){
     return (
-        <section style={{border : "solid"}}>
-            metabnb NFTs
-        </section>
+        <div className="section theme-color-bg">
+                <div id='left-section'>
+                    <div>
+                        <h2>
+                            Metabnb NFTs
+                        </h2>
+                    </div>
+                    <div id='left-section-p'>
+                        <p>Discover our NFT gift  cards collection. Loyal</p>
+                        <p>customers gets amazing gift cards which are</p>
+                        <p>traded as NFTs. These NFTs gives our customer</p>
+                        <p>access to loads of our exclusive services.</p>
+                    </div>
+                    <div className='btn-div'>
+                        <input value="Learn more" type="submit"/>
+                    </div>
+                </div>
+                <div id="nft-img-div">
+                    <img src={nftImg}/>
+                </div>
+            </div>
     );
 }
 
